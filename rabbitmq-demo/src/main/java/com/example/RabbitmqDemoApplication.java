2c384060d952b93dc88c77c206a093822f2d4608
SongQingWei <SongQingWei@sina.com>
2018-06-21 10:27:30 +0800

    first commit elasticsearch demo

---
+++ rabbitmq-demo/src/main/java/com/example/RabbitmqDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SongQingWei
+ * @date 2018年05月07 16:35
+ */
+@SpringBootApplication
+public class RabbitmqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDemoApplication.class, args);
+    }
+}
